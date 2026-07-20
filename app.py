7f974bf0336923af978bdba7a53a9712c32c00b6
willcoyle510 <s7139441@terrace.qld.edu.au>
2026-07-20 10:08:49 +1000

    commit

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import logging
 
 #---CONFIGURE APP---------------------------------------------------
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
 logging.basicConfig(filename='logs/flask.log', level=logging.INFO)
 sys.tracebacklimit = 10
 
@@ -11,22 +12,32 @@ sys.tracebacklimit = 10
 @app.route('/')
 def login():
     app.logger.info("Login")
-    return "Login Page"
+    session['permission'] = "admin"
+    return "<b>Login Page</b>"
+
+@app.route('/logout')
+def logout():
+    app.logger.info("Logout")
+    session.clear()
+    return redirect(url_for('login'))
 
 @app.route('/register')
 def register():
     app.logger.info("Register")
-    return "Registration Page"
+    return "<b>Registration Page</b>"
 
 @app.route('/home')
 def home():
+    if 'permission' in session:
+        if session['permission'] == "admin":
+            return "All glory to the administrator!"
     app.logger.info("Home")
-    return "Home Page"
+    return "<b>Home Page</b>"
 
 @app.route('/admin')
 def admin():
     app.logger.info("Admin")
-    return "Admin Page"
+    return "<b>Admin Page</b>"
 
 #main method called web server application
 if __name__ == '__main__':
